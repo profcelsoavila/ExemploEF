132d1a8cd030b7070523d59a11a44a0363d0d444
Celso de Ávila Ramos <37908497+profcelsoavila@users.noreply.github.com>
2024-03-18 20:45:10 -0300

    Atualização operações de CRUD
    Atualização do ExemploEF, aula do dia 18/03/24

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExemploEF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ExemploEF.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public Context context;
+        public ProdutoController(Context ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            //retorna uma view com todos os produtos
+            //o métoto Include carrega a associação com Fabricante
+            return View(context.Produtos
+                .Include(f => f.Fabricante));
+        }
+
+        public IActionResult Create()
+        {
+            //utiliza uma Viewbag para gerar uma lista com os nomes dos fabricantes
+            ViewBag.FabricanteID  = new SelectList(context.Fabricantes
+                .OrderBy(f => f.Nome),"FabricanteID", "Nome");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Produto produto)
+        {
+            context.Add(produto);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details (int id)
+        {
+            var produto = context.Produtos
+                .Include (f => f.Fabricante)
+                .FirstOrDefault(p => p.ProdutoID == id);
+            return View(produto);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var produto = context.Produtos.Find(id);
+            ViewBag.FabricanteID = new SelectList(context.Fabricantes.OrderBy(f => f.Nome), "FabricanteID", "Nome");
+            return View(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Produto produto)
+        {
+            //avisa a EF que o registro será modificado
+            context.Entry(produto).State = EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var produto = context.Produtos
+                .Include(f => f.Fabricante)
+                .FirstOrDefault(p => p.ProdutoID == id);
+            return View(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Produto produto)
+        {
+            context.Produtos.Remove(produto);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
